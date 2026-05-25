a13d58131ce90816338b5d611ad7e55c6717072f
Zzelen <cristian.lop.gonzalez@gmail.com>
2026-05-25 12:08:28 +0200

    chore: add pnpm workspace configuration to allow builds for specific packages

--- src/app.ts
+++ src/app.ts
@@ -46,8 +46,6 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Api online' });
 })
 
-
-
 app.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`);
 })
--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import {config} from "./env";
+
 const adapter = new PrismaLibSql({
   url: config.DATABASE_URL,
 });
+
 export const prisma = new PrismaClient({ adapter });
 
